6f0fc5b0298090e876bbeaa2a5e6d7791640be88
veaba <godpu@outlook.com>
2020-08-26 14:15:48 +0800

    docs: translate community

--- src/.vuepress/components/community/themes/index.vue
+++ src/.vuepress/components/community/themes/index.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class="themes">
+<div class="themes">
     <theme-provider v-for="provider in providers" :key="provider.name" :provider="provider" />
 
     <p>
-      Want to feature your themes here?
-      <a href="mailto:evan@vuejs.org?subject=Theme+affiliation">Contact us</a>!
+        想在这里展示你的主题吗？
+        <a href="mailto:evan@vuejs.org?subject=Theme+affiliation">联系我们</a>！
     </p>
-  </div>
+</div>
 </template>
 
 <script>
 import providers from './theme-data'
 
 export default {
-  components: {
-    ThemeProvider: () => import('./theme-provider')
-  },
+    components: {
+        ThemeProvider: () => import('./theme-provider')
+    },
 
-  data: () => ({
-    providers
-  })
+    data: () => ({
+        providers
+    })
 }
 </script>
--- src/.vuepress/components/community/themes/theme-provider.vue
+++ src/.vuepress/components/community/themes/theme-provider.vue
@@ -1,42 +1,42 @@
 <template>
-  <section>
+<section>
     <h2 :id="provider.name">{{ provider.name }}</h2>
 
-    <div class="tip custom-block description" v-html="description"/>
+    <div class="tip custom-block description" v-html="description" />
 
     <div class="themes-grid">
-      <ThemeItem v-for="theme in provider.products" :key="theme.url" :theme="theme"/>
+        <ThemeItem v-for="theme in provider.products" :key="theme.url" :theme="theme" />
     </div>
 
     <div class="see-more-container">
-      <RoundedButton :url="provider.seeMoreUrl">
-        See More Themes from {{ provider.name }}
-      </RoundedButton>
+        <RoundedButton :url="provider.seeMoreUrl">
+            查看 {{ provider.name }} 的更多主题
+        </RoundedButton>
     </div>
-  </section>
+</section>
 </template>
 
 <script>
 import showdown from 'showdown'
 
 export default {
-  props: {
-    provider: {
-      type: Object,
-      required: true
-    }
-  },
+    props: {
+        provider: {
+            type: Object,
+            required: true
+        }
+    },
 
-  components: {
-    ThemeItem: () => import('./theme-item.vue'),
-    RoundedButton: () => import('@theme/components/ui/RoundedButton.vue')
-  },
+    components: {
+        ThemeItem: () => import('./theme-item.vue'),
+        RoundedButton: () => import('@theme/components/ui/RoundedButton.vue')
+    },
 
-  computed: {
-    description () {
-      return (new showdown.Converter()).makeHtml(this.provider.description)
+    computed: {
+        description() {
+            return (new showdown.Converter()).makeHtml(this.provider.description)
+        }
     }
-  }
 }
 </script>
 
@@ -44,23 +44,23 @@ export default {
 @import "@theme/styles/_settings.scss";
 
 .themes-grid {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
 
-  @media screen and (max-width: 1300px) {
-    justify-content: center;
-  }
+    @media screen and (max-width: 1300px) {
+        justify-content: center;
+    }
 }
 
 .description {
-  margin: 1.5em 0;
+    margin: 1.5em 0;
 }
 
 .see-more {
-  &-container {
-    text-align: center;
-    width: 100%;
-  }
+    &-container {
+        text-align: center;
+        width: 100%;
+    }
 }
 </style>
