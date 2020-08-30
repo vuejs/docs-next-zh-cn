62841d5b6e38a69449805605cb887eeef0138837
Jinjiang <zhaojinjiang@me.com>
2020-08-30 22:06:41 +0800

    chore: keep most of the code and format same to the en version

--- src/.vuepress/theme/components/Sidebar.vue
+++ src/.vuepress/theme/components/Sidebar.vue
@@ -9,12 +9,12 @@
       </div>
     </div>
 
-    <NavLinks/>
+    <NavLinks />
 
-    <slot name="top"/>
+    <slot name="top" />
 
-    <SidebarLinks :depth="0" :items="items"/>
-    <slot name="bottom"/>
+    <SidebarLinks :depth="0" :items="items" />
+    <slot name="bottom" />
   </aside>
 </template>
 
@@ -43,49 +43,38 @@ export default {
 .sidebar
   .sidebar-inner
     padding 35px 0 0 20px;
-
   ul
     padding 0
     margin 0
     list-style-type none
-
   a
     display inline-block
-
   .nav-links
     display none
     border-bottom 1px solid $borderColor
     padding 0.5rem 0 0.75rem 0
-
     a
       font-weight 600
-
     .nav-item, .repo-link
       display block
       line-height 1.25rem
       font-size 1.1em
       padding 0.5rem 0 0.5rem 1.5rem
-
   & > .sidebar-links
     padding 1.5rem 0
-
     & > li > a.sidebar-link
       font-size 1.1em
       line-height 1.7
       font-weight bold
-
     & > li:not(:first-child)
       margin-top .75rem
-
 
 @media (max-width: $MQMobile)
   .sidebar
     .nav-links
       display block
-
       .dropdown-wrapper .nav-dropdown .dropdown-item a.router-link-active::after
         top calc(1rem - 2px)
-
     & > .sidebar-links
       padding 1rem 0
 </style>
