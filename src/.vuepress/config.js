82369c46d7ec2b34c500f97205b872e065f666e4
Jinjiang <zhaojinjiang@me.com>
2020-09-05 09:34:16 +0800

    feat: hide search temporarily

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -350,8 +350,8 @@ module.exports = {
     },
     smoothScroll: false,
     algolia: {
-      indexName: 'vuejs-v3',
-      apiKey: 'bc6e8acb44ed4179c30d0a45d6140d3f'
+      // indexName: 'vuejs-v3',
+      // apiKey: 'bc6e8acb44ed4179c30d0a45d6140d3f'
     }
   },
   plugins: [
