8decef1a4f48bd2e410d884748d24b7d8a412f4d
Godpu <908662421@qq.com>
2020-09-27 17:44:47 +0800

    docs: add migraion pwa support

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -216,6 +216,28 @@ module.exports = {
       rel: 'icon',
       href: '/logo.png'
     }],
+     ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        href: '/images/icons/apple-icon-152x152.png'
+      }
+    ],
+    [
+      'meta',
+      {
+        name: 'msapplication-TileImage',
+        content: '/images/icons/ms-icon-144x144.png'
+      }
+    ],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
     // localization for china
     [
       'script',
